13af0a536dab1354192c43779edc2ad58a94a2d1
Alexey Zezin <alexey.zezin@gmail.com>
2021-05-24 14:41:58 +0300

    Proof of concept

---
+++ README.md
@@ -0,0 +1,1 @@
+# Turbo_Super_Solo
---
+++ js/track.js
@@ -0,0 +1,30 @@
+$(function() {
+  let tracks = $('.track')
+  let modal = $('#track-info-modal')
+
+  tracks.each(function(index, track) {
+    $(track).click(function(event) {
+      trackObj = objTimes[$(track).attr('id')]
+      console.log(trackObj)
+      setVideo(trackObj.video)
+      stopVideo()
+      setVolume(10)
+
+      $('.modal-content > .stm-info').html('Track #' + trackObj.id)
+      $('.modal-content > .times > .stm > .time').html(trackObj.stm)
+      $('.modal-content > .times > .sg > .time').html(trackObj.sg)
+      $('.modal-content > .times > .ss > .time').html(trackObj.ss)
+      $('.modal-content > .times > .sb > .time').html(trackObj.sb)
+      $('.modal-content > .times > .rtm > .time').html(trackObj.rtm)
+
+      $(modal).show()
+    })
+  })
+
+  $(document).click((event) => {
+    if ($(event.target).hasClass('modal')) {
+      stopVideo()
+      $(modal).hide()
+    }
+  })
+});
