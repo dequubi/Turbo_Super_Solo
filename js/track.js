02fe14dda218c62e7d29a5b7cdde1d2644b3a5f4
Алексей <47535336+ShaidenUNI@users.noreply.github.com>
2021-07-18 01:58:39 +0300

    Revert "Full rewrite; STM tiers and regular solo medal times addition"

---
+++ js/track.js
@@ -0,0 +1,29 @@
+$(function() {
+  let tracks = $('.track')
+  let modal = $('#track-info-modal')
+
+  tracks.each((index, track) => {
+    $(track).click(() => {
+      trackObj = objTimes[$(track).attr('id')]
+      setVideo(trackObj.video)
+      stopVideo()
+      setVolume(10)
+
+      $('.modal-content > .stm-info').html('Track #' + trackObj.id)
+      $('.modal-content > .times > .stm > .time').html(trackObj.stm)
+      $('.modal-content > .times > .sg > .time').html(trackObj.sg)
+      $('.modal-content > .times > .ss > .time').html(trackObj.ss)
+      $('.modal-content > .times > .sb > .time').html(trackObj.sb)
+      $('.modal-content > .times > .rtm > .time').html(trackObj.rtm)
+
+      $(modal).show()
+    })
+  })
+
+  $(document).click((event) => {
+    if ($(event.target).hasClass('modal')) {
+      stopVideo()
+      $(modal).hide()
+    }
+  })
+});
