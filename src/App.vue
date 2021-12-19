56aa0be8e8318591b070ece067abe80c44c82253
Alexey Zezin <alexey.zezin@gmail.com>
2021-12-20 01:12:20 +0300

    feat: vue and tailwind init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <turbo />
+</template>
+
+<script>
+import Turbo from "./components/Turbo.vue";
+
+export default {
+  name: "App",
+  components: {
+    Turbo,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Turbo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Yo</div>
+</template>
+
+<script>
+export default {
+  name: "Turbo",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
